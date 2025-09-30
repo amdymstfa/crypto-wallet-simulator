07dba8d03de94cb50c16deefabaa329d6dbb10d8
amdymstfa <amdymoustapha011@gmail.com>
2025-09-30 14:38:11 +0100

    CRYP-56 #done: Implement Mempool management with transaction ordering and waiting time estimation
    - Added Mempool class to manage unconfirmed transactions
    - Implemented transaction insertion/removal with logging
    - Transactions are automatically sorted by fees (highest first)
    - Added getPosition() and estimateWaitingTime() to calculate queue position and expected delay
    - Improved traceability using LoggerUtil for every mempool operation

---
+++ src/main/java/model/Mempool.java
@@ -0,0 +1,63 @@
+package model ;
+
+import util.LoggerUtil;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Mempool {
+
+    private final List<Transaction> transactions ;
+    private static final int BLOCK_TIME_MINUTES = 10 ;
+
+    public Mempool(){
+        this.transactions = new ArrayList<>();
+        LoggerUtil.logInfo("Mempool initialized");
+    }
+
+    public void addTransaction(Transaction transaction){
+        transactions.add(transaction);
+        sortTransactionByFees();
+        LoggerUtil.logTransaction(transaction.getId(), "Added To Mempool", 
+            String.format("Position : %d", getPosition(transaction));
+        );
+    }
+
+    public boolean removeTransaction(Transaction transaction){
+        boolean removed = transactions.remove(transaction);
+        if (removed) {
+            LoggerUtil.logTransaction(transaction.getId(), "REMOVED_FROM_MEMPOOL", "Confirmée");
+        }
+
+        return removed ;
+    }
+
+    private void sortTransactionsByFees(){
+        transactions.sort(t1, t2) -> Double.compare(t2.getFees(), t1.getFees());
+    }
+
+    public int getPosition(Transaction transaction){
+        sortTransactionsByFees();
+        return transactions.indexOf(transaction) + 1;
+    }
+
+    public Duration estimateWaitingTime(Transaction transaction){
+        int position = getPosition(transaction);
+        long minutes = position*BLOCK_TIME_MINUTES;
+        return Duration.ofMinutes(minutes);
+    }
+
+    public List<Transaction> getTransactionsByFees(){
+        sortTransactionsByFees();
+        return new ArrayList<>(transactions);
+    }
+
+    public int size(){
+        return transactions.size();
+    }
+
+    public boolean isEmpty(){
+        return isEmpty();
+    }
+}
