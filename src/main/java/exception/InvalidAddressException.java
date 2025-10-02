381510e6bc7f3c117cadf12b4b311f2f2c8adc86
amdymstfa <amdymoustapha011@gmail.com>
2025-10-02 22:55:50 +0100

    Update InvalidAddressException.java: custom exception for invalid wallet addresses

--- src/main/java/exception/InvalidAddressException.java
+++ src/main/java/exception/InvalidAddressException.java
@@ -1,6 +1,11 @@
 package exception ;
 
 public class InvalidAddressException extends Exception {
+
+    public InvalidAddressException(String message) {
+    super(message);
+    }
+
     public InvalidAddressException(String address, String cryptoType){
         super("Invalide adress " + address + " for crypto " + cryptoType);
     }
