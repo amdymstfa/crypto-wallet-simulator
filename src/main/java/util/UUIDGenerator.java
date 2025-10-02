924efa487a51f07e20d44b9f58a1c966af2f0a7f
amdymstfa <amdymoustapha011@gmail.com>
2025-10-02 22:55:50 +0100

    Update UUIDGenerator.java: generate unique identifiers for wallets and transactions

--- src/main/java/util/UUIDGenerator.java
+++ src/main/java/util/UUIDGenerator.java
@@ -3,23 +3,21 @@ package util;
 import java.util.UUID;
 
 public class UUIDGenerator {
-    // unique ID for data base regidter 
-    public String generator(){
+
+    public static String generate() {
         return UUID.randomUUID().toString();
     }
 
-    // Id for user interface
-    public String generatorShort(){
-        return UUID.randomUUID().toString().substring(0,8);
+    public static String generateShort() {
+        return UUID.randomUUID().toString().substring(0, 8);
     }
 
-    // chech the valid format
-    public boolean isValidUUID(String uuid){
-        tyr{
-            UUID.formString(uuid);
-            return true ;
-        } catch(IllegalArgumentException e){
-            return false ;
+    public static boolean isValidUUID(String uuid) {
+        try {
+            UUID.fromString(uuid);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
         }
     }
 }
